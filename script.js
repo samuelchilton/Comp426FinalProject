4db4089e25df5d6bf267da807afd2971dd28a4dd
Samuel Chilton <stc@live.unc.edu>
2020-11-20 13:37:32 -0500

    trying axios one more time

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function(){
     const APIKEY = "a974be00eb79053c00d4b6b6372ab763a0da9c17b1c75387cc19bc5d642b268d";
-    trendingMovies(APIKEY);
+    testTrending(APIKEY);
 });
 /**
  * This function should use axios to make a GET request to the following url:
@@ -41,7 +41,7 @@ async function testTrending(APIKEY){
         //url: 'https://private-anon-08f2abf737-trakt.apiary-mock.com/movies/trending',
         url: "https://api.trakt.tv/movies/trending",
         headers: {
-            "Access-Control-Allow-Origin": "*",
+            //"Access-Control-Allow-Origin": "*",
             "Content-type": "application/json",
             "trakt-api-version": "2",
             "trakt-api-key": APIKEY,
