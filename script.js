476e69f23ff2563e85a15e0f2d931b0858e0d6d6
Samuel Chilton <stc@live.unc.edu>
2020-11-19 21:33:35 -0500

    testing axios

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello, world!");
+
+$(function(){
+    trendingMovies();
+});
+/**
+ * This function should use axios to make a GET request to the following url:
+ *   https://comp426-1fa20.cs.unc.edu/a08/heroes
+ *
+ * The axios request should be "await"ed, and once the response is available,
+ *   the body of the HTTP response (which is in JSON format) should be returned
+ *   as a JavaScript Object.
+ *
+ * @returns  {Object}  The body of the HTTP response.
+ */
+export async function trendingMovies() {
+    const result = await axios({
+        method: 'GET',
+        url: 'https://comp426-1fa20.cs.unc.edu/a08/heroes',
+    });
+    console.log(result);
+    return result.data;
+};
